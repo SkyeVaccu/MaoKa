83d0dd0ca0ededdc19d083401fa6ff93d1544289
yejiaxin <839994148@qq.com>
2020-03-19 15:28:26 +0800

    建立后端工程

---
+++ MaoKaProject/MaoKa_server-6001/src/main/java/com/example/server/MaokaServer6001Application.java
@@ -0,0 +1,13 @@
+package com.example.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaokaServer6001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaokaServer6001Application.class, args);
+    }
+
+}
